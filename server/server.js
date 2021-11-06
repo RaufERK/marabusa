4bc49dce80875f8a00a5383ea445dbdb98722a15
Rauf Erk <erk.rauf@gmail.com>
2021-11-06 17:40:08 +0300

    add path

--- server/server.js
+++ server/server.js
@@ -2,9 +2,10 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const { PORT } = process.env
+const path = require('path')
 
 app.get('*', (req, res) => {
-  res.json({ data: Math.random() })
+  res.json({ data: Math.random() , path: path.resolve('server.js')})
 })
 
 app.listen(PORT,()=>{
